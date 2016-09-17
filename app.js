1bf53e05a71180c1048ebef2e09095772bb84738
Muthui Nderitu <jmuthuin@gmail.com>
2016-09-17 06:16:38 +0300

    Added the /message get route

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var app = require("express")();
 app.get("/", function(req,res,next){
 	res.send("I am an expressjs app");
 });
+app.get("/message", function(req,res,next){
+	res.send("I am the message");
+});
 
 app.set("port", 3000 || process.env.OPENSHIFT_PORT);
 app.set("ip", "127.0.0.1" || process.env.OPENSHIFT_IP);
