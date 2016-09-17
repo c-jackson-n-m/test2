582818db597ea6cc822f8df6e9fc9f9c288c9149
Muthui Nderitu <jmuthuin@gmail.com>
2016-09-17 06:43:31 +0300

    Modified the env variables

--- app.js
+++ app.js
@@ -13,4 +13,6 @@ app.get("/girls", function(req,res,next){
 app.set("port", 3000 || process.env.OPENSHIFT_NODEJS_PORT);
 app.set("ip", "127.0.0.1" || process.env.OPENSHIFT_NODEJS_IP);
 
-app.listen(app.get("port"), app.get("ip"));
+app.listen(app.get("port"), app.get("ip"), function(){
+	console.log("Listening...");
+});
