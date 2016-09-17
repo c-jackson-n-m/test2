5e02e205be59a2c235e958002265bdd8647f70b9
Muthui Nderitu <jmuthuin@gmail.com>
2016-09-17 06:40:16 +0300

    Fixed nodejs environement variables

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.get("/girls", function(req,res,next){
 	res.send("Yeah, we have the all!");
 });
 
-app.set("port", 3000 || process.env.OPENSHIFT_PORT);
-app.set("ip", "127.0.0.1" || process.env.OPENSHIFT_IP);
+app.set("port", 3000 || process.env.OPENSHIFT_NODEJS_PORT);
+app.set("ip", "127.0.0.1" || process.env.OPENSHIFT_NODEJS_IP);
 
 app.listen(app.get("port"), app.get("ip"));
