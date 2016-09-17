cae46f1ea1ca0968a2ff652ccbf2be1d3953593d
Muthui Nderitu <jmuthuin@gmail.com>
2016-09-17 06:34:12 +0300

    Added the /girls route

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ app.get("/", function(req,res,next){
 app.get("/message", function(req,res,next){
 	res.send("I am the message");
 });
+app.get("/girls", function(req,res,next){
+	res.send("Yeah, we have the all!");
+});
 
 app.set("port", 3000 || process.env.OPENSHIFT_PORT);
 app.set("ip", "127.0.0.1" || process.env.OPENSHIFT_IP);
